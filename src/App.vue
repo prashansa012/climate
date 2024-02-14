13ce43b5d1d64d5c0b3d75b2e4af15c7919538b3
prashansa012 <156915156+prashansa012@users.noreply.github.com>
2024-02-14 16:44:52 +0530

    css of project is updated

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+<div id="App">
+  <main>
+    <div class="search-box">
+      <input type="text" class="search-bar" placeholder="Search..."/>
+    </div>
+    <div class="weather-wrap">
+      <div class="location-box">
+        <div class="location">Northampton,UK</div>
+        <div class="date">Monday 20 january 2024</div>
+      </div>
+      <div class="weather-box">
+        <div class="temp">9°c</div>
+      
+      
+        
+        <div class="weather">Rain</div>
+      </div>
+    </div>  
+  </main>
+</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      api_key: 'e5cd5bada48e5d04d1facb4852b70e1e'
+    }
+  }
+  
+
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: 'montserrat',sans-serif;
+}
+#App{
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+
+}
+main{
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom,rgba(0,0,0,0.25),rgba(0,0,0,0.75));
+}
+.search-box{
+  width: 100%;
+  margin-bottom: 30px;
+
+}
+.search-box .search-bar{
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px ;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0,0,0,0.25);
+  background-color: rgba(255,255,255,0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
+  background-color: rgba(225,225,225,0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+.location-box .location{
+    color:#fff;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: center;
+    text-shadow: 1px 3px rgba(0,0,0,0.25);
+    
+  }
+</style>
