6cf3e47650fb34877927f9479b96003fe1debf39
prashansa012 <156915156+prashansa012@users.noreply.github.com>
2024-02-14 17:46:38 +0530

    update in css, link with api of weathermap,

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,22 @@
 <div id="App">
   <main>
     <div class="search-box">
-      <input type="text" class="search-bar" placeholder="Search..."/>
+      <input 
+      type="text" 
+      class="search-bar" 
+      placeholder="Search..."
+      v-model="query"
+      @keypress="fetchWeather"
+      />
+     
     </div>
-    <div class="weather-wrap">
+    <div class="weather-wrap" v-if="typeof weather.main !='undefined'">
       <div class="location-box">
-        <div class="location">Northampton,UK</div>
+        <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
         <div class="date">Monday 20 january 2024</div>
       </div>
       <div class="weather-box">
-        <div class="temp">9°c</div>
+        <div class="temp">{{ Math.round(weather.main.temp) }}</div>
       
       
         
@@ -28,10 +35,26 @@ export default {
   name: 'App',
   data(){
     return{
-      api_key: 'e5cd5bada48e5d04d1facb4852b70e1e'
+      api_key: 'e5cd5bada48e5d04d1facb4852b70e1e',
+      url_base:'https://api.openweathermap.org/data/2.5/',
+      query:'',
+      weather:{}
     }
+  },
+  methods:{
+    fetchWeather(e){
+      if(e.key == "Enter"){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        .then(res =>{
+          return res.json();
+
+        }).then(this.setResults);
+      }
+    },
+    setResults(results){
+     this.weather = results;
+    },
   }
-  
 
 }
 </script>
@@ -92,4 +115,37 @@ main{
     text-shadow: 1px 3px rgba(0,0,0,0.25);
     
   }
+  .location-box .date{
+    color:#fff;
+    font-size: 20px;
+    font-weight: 300;
+    font-style: italic;
+    text-align: center;
+  }
+  .weather-box{
+    text-align: center;
+    
+}
+.weather-box .temp{
+display: inline-block;
+padding: 10px 25px;
+color: #fff;
+font-size: 102px;
+font-weight: 900;
+
+text-shadow: 3px 6px rgba(0,0,0,0.25);
+background-color: rgba(225,225,225,0.25);
+border-radius: 16px;
+margin: 30px 0px;
+
+box-shadow: 3px 6px rgba(0,0,0,0.25);
+
+}
+.weather-box .weather{
+color: #fff;
+font-size: 48px;
+font-weight: 700;
+font-style: italic;
+text-shadow: 3px 6px rgba(0,0,0,0.25);
+}
 </style>
